c59a377c480618a5d6d291b0bd2d848c7d92c22b
Silvia <silvia.piovesan@gmail.com>
2022-02-08 13:00:16 +0100

    setup CSS reset, global styles, variables

---
+++ src/general-styles/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    --color-blue: #312E81;
+    --color-yellow: #FDE047;
+    --color-gray: #E2E8F0;
+    --color-white: #FFFFFF;
+  }
+`;
