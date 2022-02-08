ee6526be32fa901279d40ad5470b2a1b93fc7c19
Silvia <silvia.piovesan@gmail.com>
2022-02-08 13:01:45 +0100

    feat: App styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import {Routes, Route, Link} from "react-router-dom";
 import Home from "./components/screens/Home";
 import FormBasic from "./components/screens/FormBasic";
+import { OuterContainer, StyledFooter } from "./App.styles";
+import { GlobalStyles } from "./general-styles/globalStyles";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <GlobalStyles />
+    <OuterContainer>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form-basic" element={<FormBasic />} />
       </Routes>
-      <footer>Footer</footer>
-    </div>
+      <StyledFooter>Fixed Footer</StyledFooter>
+    </OuterContainer>
+    </>
   );
 }
 
---
+++ src/App.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const OuterContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--color-gray);
+    color: var(--color-blue);
+`;
+
+export const StyledFooter = styled.footer`
+    margin-top: auto;
+    width: 100%;
+    color: var(--color-blue);
+    text-align: center;
+`;
