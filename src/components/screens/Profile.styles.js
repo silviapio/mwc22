0a4cf54e3c06ce4fbe66a579453e100ddef206ce
Silvia <silvia.piovesan@gmail.com>
2022-02-11 18:21:00 +0100

    feat: Profile styles

---
+++ src/components/screens/Profile.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ProfileOuterContainer = styled.form`
+    height: 96%;
+    overflow-y: scroll;
+    padding: 20px 10px 10px 10px;
+    border: 1px solid var(--color-white);
+    background-color: var(--color-gray);
+`;
+
+export const SkillsContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+`;
