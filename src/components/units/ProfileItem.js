403db643ceae1063bbfbdd5f724a6c878df247b5
Silvia <silvia.piovesan@gmail.com>
2022-02-11 18:19:04 +0100

    feat: create ProfileItem

---
+++ src/components/units/ProfileItem.js
@@ -0,0 +1,12 @@
+import {ItemDescription, ItemData} from "./ProfileItem.styles"
+
+function ProfileItem({descText, dataText}) {
+    return(
+        <>
+            <ItemDescription>{descText}</ItemDescription>
+            {dataText && <ItemData>{dataText}</ItemData>}
+        </>
+    );
+}
+
+export default ProfileItem;
---
+++ src/components/units/ProfileItem.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ItemDescription = styled.p`
+    font-size: 1rem;
+    font-style: italic;
+`;
+
+export const ItemData = styled.p`
+    font-size: 1.1rem;
+    font-weight: 500;
+`;
