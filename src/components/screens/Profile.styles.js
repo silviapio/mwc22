0b2961b5030783c30d50d726abb1b83ba1fb7130
Silvia <silvia.piovesan@gmail.com>
2022-02-12 10:50:49 +0100

    feat: create Profile styles in case of no data

--- src/components/screens/Profile.styles.js
+++ src/components/screens/Profile.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ProfileOuterContainer = styled.form`
     height: 96%;
-    overflow-y: scroll;
+    overflow-y: auto;
     padding: 20px 10px 10px 10px;
     border: 1px solid var(--color-white);
     background-color: var(--color-gray);
@@ -12,4 +12,12 @@ export const SkillsContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: flex-start;
+`;
+
+export const ProfileErrorContainer = styled.div`
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 25px 0;
 `;
