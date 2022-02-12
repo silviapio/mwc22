28e5587eb1d21598ac4bc580c486eb0b9ff5bf8e
Silvia <silvia.piovesan@gmail.com>
2022-02-12 19:03:23 +0100

    feat: identify returning user

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,21 +2,35 @@ import { Link } from "react-router-dom";
 import Title from "../units/Title";
 import PlainIcon from "../units/PlainIcon";
 import Button from "../units/Button";
+import { isUserRegistered } from "../../utils/localStorageUtils";
 import { WelcomeContainer, StyledHomeP } from "./Home.styles";
 import { HiOutlineThumbUp } from "react-icons/hi";
 
 function Home() {
+    const userRegistered = isUserRegistered("savedData");
+
     return(
         <>
                 <WelcomeContainer>
-                    <Title text="Welcome to MWC 22" />
-                    <StyledHomeP>Do you want to showcase your talent?</StyledHomeP>
-                    <StyledHomeP>Go ahead and create a profile!</StyledHomeP>
-                    <PlainIcon size="24px">
+                    { userRegistered ? 
+                        <>
+                        <Title text="Welcome Back!" />
+                        <StyledHomeP>Click below to see your profile</StyledHomeP>
+                        <StyledHomeP>( ͡° ͜ʖ ͡°)_/¯</StyledHomeP>
+                        </>
+                        :
+                        <>
+                        <Title text="Welcome to MWC 22" />
+                        <StyledHomeP>Do you want to showcase your talent?</StyledHomeP>
+                        <StyledHomeP>Go ahead and create a profile!</StyledHomeP>
+                        <PlainIcon size="24px">
                         <HiOutlineThumbUp />
-                    </PlainIcon>
-                    <Link to="/form">
-                        <Button text="NEW PROFILE" />
+                        </PlainIcon>
+                        </>
+                    }
+                    
+                    <Link to={userRegistered ? "/profile" : "/form"}>
+                        <Button text={userRegistered ? "SEE PROFILE" : "NEW PROFILE"} />
                     </Link>
                 </WelcomeContainer>
         </>
