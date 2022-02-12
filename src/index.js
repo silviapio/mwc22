cb08eb622ce326baff601d820353883aa5e8b72b
Silvia <silvia.piovesan@gmail.com>
2022-02-12 23:33:24 +0100

    fix: router add baseline

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/mwc22">
       <App />
     </Router>
   </React.StrictMode>,
